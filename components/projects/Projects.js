dce32a0a8e86d4662103089a0ea0f47cd249fc83
moha5145 <mms.jimma@gmail.com>
2022-07-14 12:06:25 +0200

    change frips video link

--- components/projects/Projects.js
+++ components/projects/Projects.js
@@ -9,7 +9,7 @@ import { useState } from "react";
 
 const Projects = () => {
   const { t } = useTranslation();
-  const fripsVideo = "assets/videos/Composition.mp4";
+  const fripsVideo = "https://vimeo.com/729923268";
   const [isPlayerIsVisible, setisPlayerIsVisible] = useState(false);
   const [isVideo, setisVideo] = useState(false);
 
@@ -37,6 +37,7 @@ const Projects = () => {
           ) : (
             <div className="flex flex-col justify-start w-full md:h-[750px] mb-16">
               {/* <h2 className="mb-5">{t("common:projects_t4")}</h2> */}
+              {/* <Vimeo video="729923268" autoplay /> */}
               <ReactPlayer
                 url={fripsVideo}
                 playing
