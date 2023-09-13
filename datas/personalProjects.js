ea79312af6862463fcd75736aab581ca5bd046e5
moha5145 <mms.jimma@gmail.com>
2023-09-14 00:30:09 +0200

    add new project in the lists

--- datas/personalProjects.js
+++ datas/personalProjects.js
@@ -1,6 +1,40 @@
 const personalProjects = [
+
   {
     id: "1",
+    title: "Task manager app",
+    description: "Trello like tasks manager, made with React, Express.js, MongoDB, Express.js, tailwindcss, without librarys",
+    github: "https://github.com/moha5145/my-task-manager",
+    img: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1694643568/Capture_d_%C3%A9cran_du_2023-09-14_00-18-42_o370pr.png",
+    link: "https://my-task-manager-moha.netlify.app/",
+
+    technos: [
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1656886992/techno/html_eguq4q.png" },
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1694640097/pngaaa.com-3920388_bbv93a.png" },
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1656887121/techno/react_eyoh2k.png" },
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1656887089/techno/node_duqoyr.png" },
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1656887063/techno/mongo_nml7us.png" },
+    ],
+  },
+  
+  {
+    id: "2",
+    title: "Recipe app",
+    description: "Cross platform Recipe app (web, mobile, Desktop) with local database, made with Vue 3, Quasar, Capacitor, Tauri, Localbase",
+    github: "https://github.com/moha5145/myRecipe",
+    img: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1657799390/my-recipe/Capture_d_%C3%A9cran_du_2022-07-14_13-48-12_cnwvnj.png",
+    link: "https://my-recipe-vue3.netlify.app/#/",
+
+    technos: [
+      { link: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png" },
+      { link: "https://miro.medium.com/max/256/1*RuRQuMUSDUjPCPC_PJS8tg.png" },
+      { link: "https://seeklogo.com/images/C/capacitor-logo-DF3634DD70-seeklogo.com.png" },
+      { link: "https://raw.githubusercontent.com/tauri-apps/tauri/HEAD/app-icon.png" },
+    ],
+  },
+
+  {
+    id: "3",
     title: "Todo app",
     description: "Cross platform Todo app (web, mobile, Desktop) with local database, made with Vue 3, Quasar, Capacitor, Electron, Tauri, Localbase",
     github: "https://github.com/moha5145/myTodo",
@@ -15,21 +49,6 @@ const personalProjects = [
       { link: "https://raw.githubusercontent.com/tauri-apps/tauri/HEAD/app-icon.png" },
 
       { link: "https://www.pinclipart.com/picdir/middle/570-5704154_electron-js-logo-png-clipart.png" },
-    ],
-  },
-  {
-    id: "2",
-    title: "Recipe app",
-    description: "Cross platform Recipe app (web, mobile, Desktop) with local database, made with Vue 3, Quasar, Capacitor, Tauri, Localbase",
-    github: "https://github.com/moha5145/myRecipe",
-    img: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1657799390/my-recipe/Capture_d_%C3%A9cran_du_2022-07-14_13-48-12_cnwvnj.png",
-    link: "https://my-recipe-vue3.netlify.app/#/",
-
-    technos: [
-      { link: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png" },
-      { link: "https://miro.medium.com/max/256/1*RuRQuMUSDUjPCPC_PJS8tg.png" },
-      { link: "https://seeklogo.com/images/C/capacitor-logo-DF3634DD70-seeklogo.com.png" },
-      { link: "https://raw.githubusercontent.com/tauri-apps/tauri/HEAD/app-icon.png" },
     ],
   },
 ];
