2c5c8bc4cb8052c5932d726a84a7aced5afc0c1e
moha5145 <mms.jimma@gmail.com>
2023-11-30 15:22:37 +0100

    add one project

--- datas/cloneProjects.js
+++ datas/cloneProjects.js
@@ -1,6 +1,21 @@
 const cloneProjects = [
   {
     id: "1",
+    title: "Tell-me-more",
+    description: "",
+    github: "https://github.com/moha5145/tell-me-more-front",
+    img: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1701290298/tell-me-more/form/Capture_d_%C3%A9cran_du_2023-11-29_21-35-54_kn6f9l.png",
+    link: "https://tell-me-more-moha.netlify.app/",
+    technos: [
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1656886992/techno/html_eguq4q.png" },
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1656887225/techno/sass_tnjkmf.png" },
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1656887121/techno/react_eyoh2k.png" },
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1656887089/techno/node_duqoyr.png" },
+      { link: "https://res.cloudinary.com/dyjpgdpzk/image/upload/v1656887063/techno/mongo_nml7us.png" },
+    ],
+  },
+  {
+    id: "2",
     title: "Marvel clone",
     description: "",
     github: "https://github.com/moha5145/marvel-front",
@@ -15,7 +30,7 @@ const cloneProjects = [
     ],
   },
   {
-    id: "2",
+    id: "3",
     title: "Vinted clone",
     description: "",
     github: "https://github.com/moha5145/vinted-front",
@@ -31,7 +46,7 @@ const cloneProjects = [
   },
 
   {
-    id: "3",
+    id: "4",
     title: "Tripadvisor",
     description: "",
     github: "https://github.com/moha5145/Tripadvisor-clone",
@@ -44,7 +59,7 @@ const cloneProjects = [
     ],
   },
   {
-    id: "4",
+    id: "5",
     title: "Deliveroo",
     description: "",
     github: "https://github.com/moha5145/deliveroo-front-moha",
